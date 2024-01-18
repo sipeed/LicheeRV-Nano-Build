76f18b04ffe113fe98f6d79d9c6e861f05661866
Lu <luhui@sipeed.com>
2024-01-18 14:44:05 +0800

    licheervnano: install_middleware.sh: dont replace sensor_test

--- install_middleware.sh
+++ install_middleware.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 cp -v middleware/v2/sample/*/sample_* ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/bin/
-cp -v middleware/v2/sample/*/sensor_test ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/bin/
 cp -rvf middleware/v2/sample/ive/data ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/opt/ive_data
 cp -rvf middleware/v2/sample/scene_auto/param ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/opt/scene_auto_param
 rm -v ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/bin/*.c
