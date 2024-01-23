12961100bc2b80498fd740d6b1a49c96c3c7fb2b
Lu <luhui@sipeed.com>
2024-01-23 10:44:57 +0800

    licheervnano: enable test on bootup

--- ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/opt/stress-test/stress-test-all.sh
+++ ramdisk/rootfs/overlay/cv1812cp_licheerv_nano_sd/opt/stress-test/stress-test-all.sh
@@ -6,6 +6,8 @@ killall crond
 /opt/wifi.sh
 
 nice -n 2 /opt/stress-test/net-stress-test.sh &
+nice -n 3 /opt/stress-test/audio-stress-test.sh &
+nice -n 4 /opt/stress-test/tpu-tress-test.sh &
 
 mkdir -pv /tmp/ramdisk/
 mount -t tmpfs tmpfs /tmp/ramdisk
