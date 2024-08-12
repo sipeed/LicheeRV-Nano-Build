0de38f39d1a27c3fbcb22103ef8e8d20689dbbfe
Neucrack <CZD666666@gmail.com>
2024-08-12 19:28:37 +0800

    support gadget ncm and rndis at the same time

--- build/tools/common/sd_tools/sd_gen_burn_image_rootless.sh
+++ build/tools/common/sd_tools/sd_gen_burn_image_rootless.sh
@@ -27,7 +27,7 @@ cp -fv ${output_dir}/fip.bin ${output_dir}/input/
 cp -fv ${output_dir}/rawimages/boot.sd ${output_dir}/input/rawimages/
 cp -fv ${output_dir}/rawimages/rootfs.sd ${output_dir}/input/
 touch ${output_dir}/input/usb.dev
-touch ${output_dir}/input/usb.ncm0
+touch ${output_dir}/input/usb.ncm
 touch ${output_dir}/input/wifi.sta
 touch ${output_dir}/input/gt9xx
 touch ${output_dir}/input/fb
