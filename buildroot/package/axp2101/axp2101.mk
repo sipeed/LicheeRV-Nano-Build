ebfa11ffba6ec3c297eac8b13d22c9d26841afd9
916BGAI <916772719@qq.com>
2024-08-15 16:32:37 +0800

    Add AXP2101 PMU support

---
+++ buildroot/package/axp2101/axp2101.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# axp2101
+#
+################################################################################
+
+AXP2101_VERSION = 1.0.0
+AXP2101_SITE = $(TOPDIR)/package/axp2101/src
+AXP2101_SITE_METHOD = local
+AXP2101_CFLAGS = -Iinclude -IXPowersLib -IXPowersLib/REG -Wall -Wextra
+
+define AXP2101_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) CFLAGS="$(AXP2101_CFLAGS)" -C $(@D)
+endef
+
+define AXP2101_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/axp2101 $(TARGET_DIR)/usr/bin/axp2101
+endef
+
+$(eval $(generic-package))
