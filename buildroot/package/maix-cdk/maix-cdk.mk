0e927122b9b87561ead0a42b1db789f215643eba
scpcom <scpcom@gmx.de>
2024-09-04 21:16:00 +0000

    buildroot: maix-cdk: build camera_display example

--- buildroot/package/maix-cdk/maix-cdk.mk
+++ buildroot/package/maix-cdk/maix-cdk.mk
@@ -7,11 +7,22 @@
 MAIX_CDK_VERSION = be3115270a2cb369a6a16b560cb088777add2a77
 MAIX_CDK_SITE = $(call github,sipeed,MaixCDK,$(MAIX_CDK_VERSION))
 
+MAIX_CDK_SAMPLE = camera_display
+
+MAIX_CDK_DEPENDENCIES =\
+	host-cmake \
+	host-pkgconf \
+	host-python3 \
+	host-python-pip \
+	host-python-setuptools
+
 MAIX_CDK_EXT_HOST_TOOLS = ../../../../host-tools
 MAIX_CDK_EXT_MIDDLEWARE = ../../../../middleware
 MAIX_CDK_EXT_OSDRV = ../../../../osdrv
 
 MAIX_CDK_MIDDLEWARE = components/3rd_party/sophgo-middleware/sophgo-middleware
+
+MAIX_CDK_MAIXCAM_DIST = examples/$(MAIX_CDK_SAMPLE)/dist/$(MAIX_CDK_SAMPLE)_release
 
 define MAIX_CDK_POST_EXTRACT_FIXUP
 	mv $(@D)/$(MAIX_CDK_MIDDLEWARE)/v2 $(@D)/$(MAIX_CDK_MIDDLEWARE)/v2-cdk
@@ -29,11 +40,23 @@ define MAIX_CDK_POST_EXTRACT_FIXUP
 endef
 MAIX_CDK_POST_EXTRACT_HOOKS += MAIX_CDK_POST_EXTRACT_FIXUP
 
-# todo: add real build
+# todo: build maixcam_lib from source
+define MAIX_CDK_BUILD_CMDS
+	cd $(@D)/ ; \
+	$(HOST_DIR)/bin/python3 -m pip install -r requirements.txt
+	cd $(@D)/examples/$(MAIX_CDK_SAMPLE)/ ; \
+	$(HOST_DIR)/bin/maixcdk build -p maixcam
+endef
 
 define MAIX_CDK_INSTALL_TARGET_CMDS
+	if [ ! -e ${@D}/$(MAIX_CDK_MAIXCAM_DIST)/dl_lib/libmaixcam_lib.so ] ; then \
+		rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/components/maixcam_lib/lib/libmaixcam_lib.so ${@D}/$(MAIX_CDK_MAIXCAM_DIST)/dl_lib/ ; \
+	fi
 	mkdir -pv $(TARGET_DIR)/kvmapp/kvm_system/dl_lib/
-	rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/components/maixcam_lib/lib/libmaixcam_lib.so $(TARGET_DIR)/kvmapp/kvm_system/dl_lib/
+	rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/$(MAIX_CDK_MAIXCAM_DIST)/dl_lib/libmaixcam_lib.so $(TARGET_DIR)/kvmapp/kvm_system/dl_lib/ ; \
+	mkdir -pv $(TARGET_DIR)/maixapp/$(MAIX_CDK_SAMPLE)/
+	mkdir -pv $(TARGET_DIR)/maixapp/tmp
+	rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/$(MAIX_CDK_MAIXCAM_DIST)/ $(TARGET_DIR)/maixapp/$(MAIX_CDK_SAMPLE)/
 	#rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(MAIX_CDK_PKGDIR)/overlay/ $(TARGET_DIR)/
 endef
 
