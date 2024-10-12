b8b30346cd24878d7ccdab75300dc209d6b2e1ce
scpcom <scpcom@gmx.de>
2024-10-12 05:40:44 +0200

    buildroot: nanokvm-server: maixcam_lib: copy from middleware/sample/test_mmf

--- buildroot/package/nanokvm-sg200x/nanokvm-sg200x.mk
+++ buildroot/package/nanokvm-sg200x/nanokvm-sg200x.mk
@@ -9,8 +9,10 @@ NANOKVM_SG200X_BASE = $(NANOKVM_SG200X_VERSION)
 NANOKVM_SG200X_SOURCE = $(NANOKVM_SG200X_BASE).zip
 NANOKVM_SG200X_SITE = https://cdn.sipeed.com/nanokvm
 
-NANOKVM_SG200X_DEPENDENCIES += maix-cdk nanokvm-server
+NANOKVM_SG200X_DEPENDENCIES += nanokvm-server
 
+NANOKVM_SG200X_EXT_MIDDLEWARE = ../../../../middleware
+NANOKVM_SG200X_EXT_MAIXCAM_LIB = v2/sample/test_mmf/maixcam_lib/release.linux/libmaixcam_lib.so
 NANOKVM_SG200X_EXT_OVERLAY = $(BR2_ROOTFS_OVERLAY)
 
 define NANOKVM_SG200X_EXTRACT_CMDS
@@ -27,6 +29,9 @@ define NANOKVM_SG200X_INSTALL_TARGET_CMDS
 	rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/kvmapp/ $(TARGET_DIR)/kvmapp/
 	echo -n 720 > $(TARGET_DIR)/kvmapp/kvm/res
 	echo 30 > $(TARGET_DIR)/kvmapp/kvm/fps
+	if [ -e ${@D}/$(NANOKVM_SG200X_EXT_MIDDLEWARE)/$(NANOKVM_SG200X_EXT_MAIXCAM_LIB) ]; then \
+		rsync -r --verbose --copy-dirlinks --copy-links --hard-links ${@D}/$(NANOKVM_SG200X_EXT_MIDDLEWARE)/$(NANOKVM_SG200X_EXT_MAIXCAM_LIB) $(TARGET_DIR)/kvmapp/kvm_system/dl_lib/ ; \
+	fi
 	rm -f $(TARGET_DIR)/kvmapp/system/ko/*.ko
 	if [ "X$(BR2_PACKAGE_TAILSCALE_RISCV64)" != "Xy" ]; then \
 		rm -f $(TARGET_DIR)/kvmapp/system/init.d/S??tailscaled ; \
