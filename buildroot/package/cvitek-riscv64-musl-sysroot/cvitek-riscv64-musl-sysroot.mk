176e78b4d3323e740cefb709e81b00aa5e8564c8
scpcom <scpcom@gmx.de>
2024-12-29 18:06:55 +0100

    cvitek-riscv64-musl-sysroot: TOOLCHAIN_EXTERNAL_BIN to get sysroot path

--- buildroot/package/cvitek-riscv64-musl-sysroot/cvitek-riscv64-musl-sysroot.mk
+++ buildroot/package/cvitek-riscv64-musl-sysroot/cvitek-riscv64-musl-sysroot.mk
@@ -4,19 +4,19 @@
 #
 ################################################################################
 
-CVITEK_RISCV64_MUSL_SYSROOT_VERSION = fb0a6ac7409fb477c5f46ced75bf05527def7cb9
-CVITEK_RISCV64_MUSL_SYSROOT_SITE = $(call github,0x754C,cvitek-riscv64-musl-sysroot,$(CVITEK_RISCV64_MUSL_SYSROOT_VERSION))
+CVITEK_RISCV64_MUSL_SYSROOT_VERSION = 1.0.0
+CVITEK_RISCV64_MUSL_SYSROOT_SITE = $(TOPDIR)/package/cvitek-riscv64-musl-sysroot
+CVITEK_RISCV64_MUSL_SYSROOT_SITE_METHOD = local
 
-CVITEK_EXT_HOST_TOOLS = ../../../../host-tools
-CVITEK_EXT_SYSROOT_LIB = gcc/riscv64-linux-musl-x86_64/sysroot/lib
-
-define CVITEK_RISCV64_MUSL_SYSROOT_EXTRACT_CMDS
+define CVITEK_RISCV64_MUSL_SYSROOT_BUILD_CMDS
+	rm -f $(@D)/Config*
+	rm -f $(@D)/*.mk
 	mkdir -pv $(@D)/usr/lib/
-	if [ -e $(@D)/$(CVITEK_EXT_HOST_TOOLS)/$(CVITEK_EXT_SYSROOT_LIB) ]; then \
-		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(@D)/$(CVITEK_EXT_HOST_TOOLS)/$(CVITEK_EXT_SYSROOT_LIB)/ld-*.so* $(@D)/usr/lib/ ; \
-	fi
 	if [ ! -e $(@D)/lib ]; then \
 		ln -s usr/lib $(@D)/lib ; \
+	fi
+	if [ -e $(realpath $(TOOLCHAIN_EXTERNAL_BIN)../sysroot/lib) ]; then \
+		rsync -r --verbose --copy-dirlinks --copy-links --hard-links $(realpath $(TOOLCHAIN_EXTERNAL_BIN)../sysroot/lib)/ld-*.so* $(@D)/usr/lib/ ; \
 	fi
 endef
 
